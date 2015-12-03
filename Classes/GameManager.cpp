ba3cfcfea052d8d57142857d99d345002c757aa4
CurlyWhirly <s030822c@student.staffs.ac.uk>
2015-12-03 11:40:38 +0000

    s030822c - Added GameManager.cpp code

---
+++ Classes/GameManager.cpp
@@ -0,0 +1,39 @@
+#include "GameManager.h"
+
+//Set the instance to be null.
+GameManager* GameManager::instance = NULL;
+
+GameManager* GameManager::sharedGameManager()
+{
+	if (instance == NULL)
+	{
+		instance = new GameManager();
+	}
+
+	return instance;
+}
+
+GameManager::GameManager()
+{
+	isGameLive = false;
+}
+
+GameManager::~GameManager()
+{
+
+}
+
+void GameManager::AddToScore(int increment)
+{
+	this->score += increment;
+}
+
+void GameManager::ResetScore()
+{
+	score = 0;
+}
+
+int GameManager::GetScore()
+{
+	return score;
+}
