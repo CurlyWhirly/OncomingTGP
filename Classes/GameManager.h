a9c934ace7b53aa70467f9df5fdef3ca44c042f0
CurlyWhirly <s030822c@student.staffs.ac.uk>
2015-12-03 11:37:50 +0000

    s030822c - Added GameManager.h code

---
+++ Classes/GameManager.h
@@ -0,0 +1,27 @@
+#ifndef GameManager_h
+#define GameManager_h
+
+#include <stdio.h>
+
+class GameManager
+{
+public:
+	~GameManager();
+	static GameManager* sharedGameManager();
+
+	//Score accessors.
+	void AddToScore(int increment);
+	void ResetScore();
+	int  GetScore();
+
+	//Accessible variable
+	bool isGameLive;
+
+private:
+	GameManager();
+	static GameManager* instance;
+
+	int score;
+};
+
+#endif
