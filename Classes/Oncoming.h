899434ca4f7386ecc6cc020a953dc6dc9ec1e611
CurlyWhirly <s030822c@student.staffs.ac.uk>
2015-12-03 11:48:54 +0000

    s030822c - Changed class name to Oncoming

---
+++ Classes/Oncoming.h
@@ -0,0 +1,19 @@
+#ifndef __ONCOMING_GAME_H__
+#define __ONCOMING_GAME_H__
+
+#include "cocos2d.h"
+
+class Oncoming : public cocos2d::Layer
+{
+public:
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::Scene* createScene();
+
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();
+
+    // implement the "static create()" method manually
+    CREATE_FUNC(Oncoming);
+};
+
+#endif // __ONCOMING_GAME_H__
