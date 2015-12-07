bbce339fad552ef39088a4ba67770301c4b59e4b
b015698c <b015698c@student.staffs.ac.uk>
2015-12-07 14:21:41 +0000

    b015698c - Player.h added

---
+++ Classes/Player.h
@@ -0,0 +1,22 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include "cocos2d.h"
+#include "ui/CocosGUI.h"
+
+class Player : public cocos2d::Node
+{
+public:
+	Player();
+	~Player();
+
+	virtual bool init() override;
+	static Player* create();
+
+	void Update(float);
+
+private:
+	cocos2d::Sprite* player;
+};
+
+#endif
